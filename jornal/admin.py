117cc024d25d115dc274d3b9f5dd370b94d5e3f0
Rafael Costa <Rafael Costa>
2026-04-14 00:10:25 +0100

    Categorias de notícias e relações de banco de dados

--- jornal/admin.py
+++ jornal/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Artigo, Comentario
+from .models import Artigo, Comentario, Categoria
 
 admin.site.register(Artigo)
 admin.site.register(Comentario)
+admin.site.register(Categoria)
--- jornal/models.py
+++ jornal/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    
+    def __str__(self): 
+        return self.nome
+
 class Artigo(models.Model):
     titulo = models.CharField(max_length=200)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
     corpo = models.TextField()
     data_publicacao = models.DateTimeField(auto_now_add=True)
 
@@ -15,3 +22,4 @@ class Comentario(models.Model):
 
     def __str__(self):
         return f"Comentário em {self.artigo.titulo}"
+
